438b473bacb561dbbc7ef01ae55b8381b76d37a3
Kyle <kylef@myyahoo.com>
2025-02-20 22:09:23 -0500

    Initial commit

---
+++ src/main/kotlin/com/kyfstore/mcversionrenamer/MCVersionRenamerClient.kt
@@ -0,0 +1,9 @@
+﻿package com.kyfstore.mcversionrenamer
+
+import net.fabricmc.api.ClientModInitializer
+
+class MCVersionRenamerClient : ClientModInitializer {
+    override fun onInitializeClient() {
+
+    }
+}
